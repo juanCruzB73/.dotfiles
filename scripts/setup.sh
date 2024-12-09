8e7d4e9904b45a68e7702e040910251046dacb38
juanb <juancruzberrios73@gmail.com>
2024-12-09 16:37:28 -0300

    me vot a m

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,12 @@ if [ "$(id -u)" -ne "0" ]; then
     exit 1
 fi
 
+# Ensure .dotfiles directory exists
+if [ ! -d ~/.dotfiles ]; then
+    echo "~/.dotfiles directory does not exist!"
+    exit 1
+fi
+
 # Update package list
 echo "Updating packages..."
 apt update -y
@@ -33,7 +39,7 @@ echo "Installing Node.js..."
 nvm install node
 
 # Install Postman if not already installed
-if ! dpkg -l | grep -q postman; then
+if ! snap list | grep -q postman; then
     echo "Installing Postman..."
     sudo snap install postman
 else
@@ -44,10 +50,47 @@ fi
 echo "Installing i3 utilities..."
 apt install -y i3lock i3status dmenu
 
+# Install Visual Studio Code if not already installed
+if ! command -v code &> /dev/null; then
+    echo "Installing Visual Studio Code..."
+    sudo snap install --classic code
+    if ! command -v code &> /dev/null; then
+        echo "VS Code installation failed."
+        exit 1
+    fi
+else
+    echo "VS Code already installed."
+fi
+
 # Set up PostgreSQL
 echo "Setting up PostgreSQL..."
-sudo -u postgres psql -c "CREATE USER jcadmin WITH PASSWORD 'admin';"
-sudo -u postgres psql -c "CREATE DATABASE mydb;"
+sudo -u postgres psql -c "DO \$(echo \"SELECT 1 FROM pg_roles WHERE rolname='jcadmin' LIMIT 1\") IS NULL DO CREATE USER jcadmin WITH PASSWORD 'admin';"
+sudo -u postgres psql -c "DO \$(echo \"SELECT 1 FROM pg_database WHERE datname='mydb' LIMIT 1\") IS NULL DO CREATE DATABASE mydb;"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE mydb TO jcadmin"
 
-echo "SET UP COMPLETE"
+# Setting up VSCode dotfiles
+echo "Setting up VSCode dotfiles..."
+ln -sf ~/.dotfiles/.vscode/settings.json ~/.config/Code/User/settings.json
+ln -sf ~/.dotfiles/.vscode/keybindings.json ~/.config/Code/User/keybindings.json
+
+# Install VSCode extensions if extensions.json exists
+if [ -f ~/.dotfiles/.vscode/extensions/extensions.json ]; then
+    echo "Installing VSCode extensions..."
+    while IFS= read -r extension; do
+        code --install-extension "$extension"
+    done < ~/.dotfiles/.vscode/extensions/extensions.json
+else
+    echo "No extensions.json found. Skipping extension installation."
+fi
+
+# Setting up dotfiles
+echo "Setting up dotfiles..."
+if [ -f ~/.dotfiles/.gitconfig ]; then
+    ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+else
+    echo ".gitconfig not found in ~/.dotfiles."
+fi
+if [ -f ~/.dotfiles/.config/i3/config ]; then
+    ln -sf ~/.dotfiles/.config/i3/config ~/.config/i3/config
+else
+    echo "i3 config not f
